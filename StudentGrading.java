a2291d675892bfba3a1c8adebb1ce4157568e827
Merve-die <gracekanyonyo8@gmail.com>
2025-10-19 19:55:38 +0300

    Add files via upload

---
+++ StudentGrading.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vu.studentgrading;
+import java.util.Scanner;
+
+public class StudentGrading {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter student's score (0 - 100): ");
+        int score = input.nextInt();
+
+        int grade;
+        String remark;
+
+        if (score >= 80 && score <= 100) {
+            grade = 1; remark = "Distinction";
+        } else if (score >= 75) {
+            grade = 2; remark = "Distinction";
+        } else if (score >= 66) {
+            grade = 3; remark = "Credit";
+        } else if (score >= 60) {
+            grade = 4; remark = "Credit";
+        } else if (score >= 50) {
+            grade = 5; remark = "Credit";
+        } else if (score >= 45) {
+            grade = 6; remark = "Credit";
+        } else if (score >= 35) {
+            grade = 7; remark = "Pass";
+        } else if (score >= 30) {
+            grade = 8; remark = "Pass";
+        } else {
+            grade = 9; remark = "Fail";
+        }
+
+        System.out.printf("Score: %d\nGrade: %d\nRemark: %s\n", score, grade, remark);
+        input.close();
+    }
+}
+
+
